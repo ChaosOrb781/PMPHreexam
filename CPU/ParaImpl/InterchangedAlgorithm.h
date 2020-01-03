b60f28bcaa5f75728ed24cc82e532607ca5023b5
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-03 17:21:21 +0100

    Simple interchange

--- CPU/ParaImpl/InterchangedAlgorithm.h
+++ CPU/ParaImpl/InterchangedAlgorithm.h
@@ -15,32 +15,27 @@ int   run_Interchanged(
                 const REAL   beta,
                       REAL*  res   // [outer] RESULT
 ) {
-	//cout << "Starting interchange algorithm... " << endl;
 	vector<PrivGlobs> globstastic;
 	for( unsigned i = 0; i < outer; ++ i ) {
-		//cout << "Starting initialization of " << i << endl;
 		globstastic.push_back(PrivGlobs(numX, numY, numY));
 		initGrid(s0,alpha,nu,t, numX, numY, numT, globstastic[i]);
 		initOperator(globstastic[i].myX,globstastic[i].myDxx);
 		initOperator(globstastic[i].myY,globstastic[i].myDyy);
 		REAL strike = 0.001*i;
 		setPayoff(strike, globstastic[i]);
-		//cout << "Ended initialization of " << i << endl;
 	}
-	for( unsigned i = 0; i < outer; ++ i ) {
-		//cout << "Started process for " << i << endl;
-		for(int j = numT-2;j>=0;--j)
+	for(int j = numT-2;j>=0;--j)
 		{
-			//cout << "   Subprocess " << j << endl;
-			updateParams(j,alpha,beta,nu,globstastic[i]);
-			rollback(j, globstastic[i]);
+		for( unsigned i = 0; i < outer; ++ i ) {
+			{
+				updateParams(j,alpha,beta,nu,globstastic[i]);
+				rollback(j, globstastic[i]);
+			}
 		}
-		//cout << "Ended process for " << i << endl;
     }
 	for( unsigned i = 0; i < outer; ++ i ) {
         res[i] = globstastic[i].myResult[globstastic[i].myXindex][globstastic[i].myYindex];
     }
-	//cout << "Ending interchange algorithm... " << endl;
     return 1;
 }
 
