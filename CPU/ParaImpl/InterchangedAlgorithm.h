893516b25296a8e6a5f46118cbf39911e9a3aa7f
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-03 10:52:30 +0100

    Simple in-lining

--- CPU/ParaImpl/InterchangedAlgorithm.h
+++ CPU/ParaImpl/InterchangedAlgorithm.h
@@ -3,53 +3,38 @@
 
 #include "OriginalAlgorithm.h"
 
-REAL   value_interchanged(
-  int outer_i,
-  const REAL s0,
-	//const REAL strike,
-	const REAL t,
-	const REAL alpha,
-	const REAL nu,
-	const REAL beta,
-	const unsigned int numX,
-	const unsigned int numY,
-	const unsigned int numT,
-  const unsigned int outer,
-  REAL* res
+int   run_Interchanged(  
+                const uint   outer,
+                const uint   numX,
+                const uint   numY,
+                const uint   numT,
+                const REAL   s0,
+                const REAL   t, 
+                const REAL   alpha, 
+                const REAL   nu, 
+                const REAL   beta,
+                      REAL*  res   // [outer] RESULT
 ) {
+    REAL strike;
+    PrivGlobs    globs(numX, numY, numT);
 
-	for (int i = 0; i < outer; ++i)
-	{
-    PrivGlobs globs(numX, numY, numT);
-    initGrid(s0, alpha, nu, t, numX, numY, numT, globs);
-    initOperator(globs.myX, globs.myDxx);
-    initOperator(globs.myY, globs.myDyy);
-    REAL strike = 0.001 * outer_i;
-    setPayoff(strike, globs);
-		updateParams(i, alpha, beta, nu, globs);
-		rollback(i, globs);
-    res[outer_i] = globs.myResult[globs.myXindex][globs.myYindex];
-	}
-}
+    for( unsigned i = 0; i < outer; ++ i ) {
+        strike = 0.001*i;
+		initGrid(s0,alpha,nu,t, numX, numY, numT, globs);
+		initOperator(globs.myX,globs.myDxx);
+		initOperator(globs.myY,globs.myDyy);
 
-int   run_Interchanged(
-	const uint   outer,
-	const uint   numX,
-	const uint   numY,
-	const uint   numT,
-	const REAL   s0,
-	const REAL   t,
-	const REAL   alpha,
-	const REAL   nu,
-	const REAL   beta,
-	REAL* res   // [outer] RESULT
-) {
-	for (unsigned i = numT - 2; i >= 0; --i) {
-		value_interchanged(i, s0, t,
-			alpha, nu, beta,
-			numX, numY, numT, outer, res);
-	}
-	return 1;
+		setPayoff(strike, globs);
+		for(int j = globs.myTimeline.size()-2;j>=0;--j)
+		{
+			updateParams(i,alpha,beta,nu,globs);
+			rollback(i, globs);
+		}
+
+		return 
+        res[i] = globs.myResult[globs.myXindex][globs.myYindex];
+    }
+    return 1;
 }
 
 #endif
