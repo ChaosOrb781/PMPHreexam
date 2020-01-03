1802a94c5c2767b4f4a4f87258a41c75ec913f1c
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-03 12:08:23 +0100

    Removed one expansion

--- CPU/ParaImpl/InterchangedAlgorithm.h
+++ CPU/ParaImpl/InterchangedAlgorithm.h
@@ -16,26 +16,24 @@ int   run_Interchanged(
                       REAL*  res   // [outer] RESULT
 ) {
 
-	PrivGlobs* globstastic = (PrivGlobs*) malloc(sizeof(PrivGlobs) * outer * numT);
+	PrivGlobs* globstastic = (PrivGlobs*) malloc(sizeof(PrivGlobs) * outer);
 	
 	for( unsigned i = 0; i < outer; ++ i ) {
-		for( unsigned j = 0; j < numT; ++ j ) {
-			PrivGlobs globs(numX, numY, numY);
-			initGrid(s0,alpha,nu,t, numX, numY, numT, globs);
-			initOperator(globs.myX,globs.myDxx);
-			initOperator(globs.myY,globs.myDyy);
-			REAL strike = 0.001*i;
-			setPayoff(strike, globs);
-			globstastic[i * numT + j] = globs;
-		}
+		PrivGlobs globs(numX, numY, numY);
+		initGrid(s0,alpha,nu,t, numX, numY, numT, globs);
+		initOperator(globs.myX,globs.myDxx);
+		initOperator(globs.myY,globs.myDyy);
+		REAL strike = 0.001*i;
+		setPayoff(strike, globs);
+		globstastic[i] = globs;
 	}
 	for( unsigned i = 0; i < outer; ++ i ) {
 		for(int j = numT-2;j>=0;--j)
 		{
-			updateParams(j,alpha,beta,nu,globstastic[i * numT + j]);
-			rollback(j, globstastic[i * numT + j]);
+			updateParams(j,alpha,beta,nu,globstastic[i]);
+			rollback(j, globstastic[i]);
 		}
-        res[i] = globstastic[i * numT + j].myResult[globstastic[i * numT + j].myXindex][globstastic[i * numT + j].myYindex];
+        res[i] = globstastic[i].myResult[globstastic[i].myXindex][globstastic[i].myYindex];
     }
 	free(globstastic);
     return 1;
