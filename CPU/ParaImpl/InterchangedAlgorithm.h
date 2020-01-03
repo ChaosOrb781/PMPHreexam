9cf71482a3d31cc8359039530d870a431f6c5369
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-03 17:10:35 +0100

    Trying to readd the body in other loop

--- CPU/ParaImpl/InterchangedAlgorithm.h
+++ CPU/ParaImpl/InterchangedAlgorithm.h
@@ -19,27 +19,27 @@ int   run_Interchanged(
 	cout << "Starting interchange algorithm... " << endl;
 	vector<PrivGlobs> globstastic;
 	for( unsigned i = 0; i < outer; ++ i ) {
-		cout << "Starting initialization of " << i << endl;
+		//cout << "Starting initialization of " << i << endl;
 		globstastic.push_back(PrivGlobs(numX, numY, numY));
-		cout << "Ended initialization of " << i << endl;
-	}
-	for( unsigned i = 0; i < outer; ++ i ) {
-		cout << "Started process for " << i << endl;
 		initGrid(s0,alpha,nu,t, numX, numY, numT, globstastic[i]);
 		initOperator(globstastic[i].myX,globstastic[i].myDxx);
 		initOperator(globstastic[i].myY,globstastic[i].myDyy);
 		REAL strike = 0.001*i;
 		setPayoff(strike, globstastic[i]);
+		//cout << "Ended initialization of " << i << endl;
+	}
+	for( unsigned i = 0; i < outer; ++ i ) {
+		//cout << "Started process for " << i << endl;
 		for(int j = numT-2;j>=0;--j)
 		{
-			cout << "   Subprocess " << j << endl;
+			//cout << "   Subprocess " << j << endl;
 			updateParams(j,alpha,beta,nu,globstastic[i]);
 			rollback(j, globstastic[i]);
 		}
-		cout << "Ended process for " << i << endl;
+		//cout << "Ended process for " << i << endl;
         res[i] = globstastic[i].myResult[globstastic[i].myXindex][globstastic[i].myYindex];
     }
-	cout << "Ending interchange algorithm... " << endl;
+	//cout << "Ending interchange algorithm... " << endl;
     return 1;
 }
 
