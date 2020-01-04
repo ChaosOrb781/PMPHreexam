01bae0579c0ff36fd3a44726b7558541febba45a
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-04 18:54:32 +0100

    Added parallel interchanged version, might not work with push_back

--- CPU/ParaImpl/InterchangedAlgorithm.h
+++ CPU/ParaImpl/InterchangedAlgorithm.h
@@ -2,6 +2,7 @@
 #define INTERCHANGED_ALGORITHM
 
 #include "OriginalAlgorithm.h"
+#include <omp.h>
 
 int   run_Interchanged(  
                 const uint   outer,
@@ -33,6 +34,50 @@ int   run_Interchanged(
 		}
     }
 	for( unsigned i = 0; i < outer; ++ i ) {
+        res[i] = globstastic[i].myResult[globstastic[i].myXindex][globstastic[i].myYindex];
+    }
+    return 1;
+}
+
+int   run_InterchangedParallel(  
+                const uint   outer,
+                const uint   numX,
+                const uint   numY,
+                const uint   numT,
+                const REAL   s0,
+                const REAL   t, 
+                const REAL   alpha, 
+                const REAL   nu, 
+                const REAL   beta,
+                      REAL*  res   // [outer] RESULT
+) {
+	int procs = 0;
+	
+	vector<PrivGlobs> globstastic;
+#pragma omp parallel for
+	for( unsigned i = 0; i < outer; ++ i ) {
+		globstastic.push_back(PrivGlobs(numX, numY, numY));
+		initGrid(s0,alpha,nu,t, numX, numY, numT, globstastic[i]);
+		initOperator(globstastic[i].myX,globstastic[i].myDxx);
+		initOperator(globstastic[i].myY,globstastic[i].myDyy);
+		REAL strike = 0.001*i;
+		setPayoff(strike, globstastic[i]);
+	}
+	for(int j = 0;j<=numT-2;++j) {
+#pragma omp parallel for
+		for( unsigned i = 0; i < outer; ++ i ) {
+			{
+				updateParams(j,alpha,beta,nu,globstastic[i]);
+				rollback(j, globstastic[i]);
+			}
+		}
+    }
+#pragma omp parallel for
+	for( unsigned i = 0; i < outer; ++ i ) {
+		{
+            int th_id = omp_get_thread_num();
+            if(th_id == 0) { procs = omp_get_num_threads(); }
+        }
         res[i] = globstastic[i].myResult[globstastic[i].myXindex][globstastic[i].myYindex];
     }
     return 1;
