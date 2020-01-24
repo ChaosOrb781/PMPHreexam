b1a733f62a7c3e77e2e21ea025327bd897d503d0
Tobias Floor <sxr297@alumni.ku.dk>
2020-01-24 09:47:30 +0100

    Minor corrections

--- CPU/ParaImpl/ProjectMain.cpp
+++ CPU/ParaImpl/ProjectMain.cpp
@@ -12,6 +12,7 @@
 #define RUN_CPU_EXPERIMENTAL true
 
 typedef int (*fun)(const uint, const uint, const uint, const uint, const REAL, const REAL, const REAL, const REAL, const REAL, REAL*);
+typedef int (*funT)<int>(const uint, const uint, const uint, const uint, const REAL, const REAL, const REAL, const REAL, const REAL, REAL*);
 
 bool compare_validate(REAL* result, REAL* expected, uint size) {
     bool isvalid = true;
@@ -26,8 +27,8 @@ bool compare_validate(REAL* result, REAL* expected, uint size) {
     return isvalid;
 }
 
-template <int blocksize = 0>
-ReturnStat* RunStatsOnProgram(const char* name, fun f, 
+template <typename ftype, int blocksize = 0>
+ReturnStat* RunStatsOnProgram(const char* name, ftype f, 
     REAL* res, const uint outer, const uint numX, const uint numY, const uint numT, 
     const REAL s0, const REAL t, const REAL alpha, const REAL nu, const REAL beta) 
     {
@@ -35,26 +36,23 @@ ReturnStat* RunStatsOnProgram(const char* name, fun f,
     printf("\n[Running %-15s, outer: %3d, X: %3d, Y: %3d, T: %3d]\n", name, outer, numX, numY, numT);
     struct timeval t_start, t_end, t_diff;
     gettimeofday(&t_start, NULL);
-
-    int procs = 0;
-    if (blocksize > 0) {
-        procs = f<blocksize>(outer, numX, numY, numT, s0, t, alpha, nu, beta, res);
-    }
-    else 
-    {
+    
+    if (ftype == typeof(fun))
         procs = f(outer, numX, numY, numT, s0, t, alpha, nu, beta, res);
-    }
+    else if (ftype == typeof(funT))
+        procs = f<blocksize>(outer, numX, numY, numT, s0, t, alpha, nu, beta, res);
 
     gettimeofday(&t_end, NULL);
     timeval_subtract(&t_diff, &t_end, &t_start);
     return new ReturnStat(t_diff.tv_sec*1e6+t_diff.tv_usec, procs);
 }
 
-template <int blocksize = 0>
+
+template <typename ftype, int blocksize = 0>
 void RunTestOnProgram(const char* title, fun f, REAL* expected, ReturnStat* expectedStats, const uint outer, const uint numX, const uint numY, const uint numT,
 	const REAL s0, const REAL t, const REAL alpha, const REAL nu, const REAL beta) {
 	REAL* res = (REAL*)malloc(outer * sizeof(REAL));
-	ReturnStat* returnstatus = RunStatsOnProgram<blocksize>(title, f, res, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+	ReturnStat* returnstatus = RunStatsOnProgram<ftype, blocksize>(title, f, res, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 	bool is_valid = compare_validate(res, expected, outer);
 	writeStatsAndResult(is_valid, res, outer, false, returnstatus, expectedStats);
 }
@@ -76,24 +74,24 @@ int main()
     // If initial original program is correct, run rest
     if (is_valid) {
         //Simple parallized programs
-        RunTestOnProgram("Simple Parallel", run_SimpleParallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+        RunTestOnProgram<fun>("Simple Parallel", run_SimpleParallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 
 #if RUN_CPU_EXPERIMENTAL || RUN_ALL
-        RunTestOnProgram("Simple Parallel Static", run_SimpleParallelStatic, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
-		RunTestOnProgram("Simple Parallel Dynamic", run_SimpleParallelDynamic, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+        RunTestOnProgram<fun>("Simple Parallel Static", run_SimpleParallelStatic, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+		RunTestOnProgram<fun>("Simple Parallel Dynamic", run_SimpleParallelDynamic, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 #endif
 
 
-		RunTestOnProgram("Interchanged", run_Interchanged, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+		RunTestOnProgram<fun>("Interchanged", run_Interchanged, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 #if RUN_CPU_EXPERIMENTAL || RUN_ALL
-        RunTestOnProgram("Interchanged Optimized", run_InterchangedAlternative, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+        RunTestOnProgram<fun>("Interchanged Optimized", run_InterchangedAlternative, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 #endif  
-		RunTestOnProgram("Parallel Interchanged", run_InterchangedParallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+		RunTestOnProgram<fun>("Parallel Interchanged", run_InterchangedParallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 #if RUN_CPU_EXPERIMENTAL || RUN_ALL
-        RunTestOnProgram("Parallel Interchanged Optimized", run_InterchangedParallelAlternative, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+        RunTestOnProgram<fun>("Parallel Interchanged Optimized", run_InterchangedParallelAlternative, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
 #endif  
 
-        RunTestOnProgram<16>("Kernelized", run_Kernelized, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
+        RunTestOnProgram<funT, 16>("Kernelized", run_Kernelized, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta);
     }
 
     return 0;
