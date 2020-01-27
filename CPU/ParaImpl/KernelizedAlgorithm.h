a34b208325daaa192b36114ffb4b338c8666fc54
Chaos_Orb <sxr297@alumni.ku.dk>
2020-01-27 13:50:32 +0100

    Now printing processors correctly

--- CPU/ParaImpl/KernelizedAlgorithm.h
+++ CPU/ParaImpl/KernelizedAlgorithm.h
@@ -592,6 +592,8 @@ int   run_SimpleKernelized_Parallel(
                 const uint   blocksize,
                       REAL*  res   // [outer] RESULT
 ) {
+    int procs = 0;
+
 	vector<REAL> myX(numX);       // [numX]
     vector<REAL> myY(numY);       // [numY]
     vector<REAL> myTimeline(numT);// [numT]
@@ -638,7 +640,12 @@ int   run_SimpleKernelized_Parallel(
 	rollback_Kernel_para(outer, numT, numX, numY, myTimeline, myDxx, myDyy, myVarX, myVarY, myResult);
 	
     //cout << "Test7" << endl;
+#pragma omp parallel for schedule(static)
 	for(uint i = 0; i < outer; i++) {
+        {
+            int th_id = omp_get_thread_num();
+            if(th_id == 0) { procs = omp_get_num_threads(); }
+        }
         res[i] = myResult[((i * numX) + myXindex) * numY + myYindex];
     }
 
@@ -656,19 +663,19 @@ int   run_SimpleKernelized_Parallel(
     for (int i = 0; i < numX; i ++) {
         if (abs(myX[i] - TestmyX[i]) > 0.00001f) {
             cout << "myX[" << i << "] did not match! was " << myX[i] << " expected " << TestmyX[i] << endl;
-            return 1;
+            return procs;
         }
     }
     for (int i = 0; i < numY; i ++) {
         if (abs(myY[i] - TestmyY[i]) > 0.00001f) {
             cout << "myY[" << i << "] did not match! was " << myY[i] << " expected " << TestmyY[i] << endl;
-            return 1;
+            return procs;
         }
     }
     for (int i = 0; i < numT; i ++) {
         if (abs(myTimeline[i] - TestmyTimeline[i]) > 0.00001f) {
             cout << "myTimeline[" << i << "] did not match! was " << myTimeline[i] << " expected " << TestmyTimeline[i] << endl;
-            return 1;
+            return procs;
         }
     }
 
@@ -677,7 +684,7 @@ int   run_SimpleKernelized_Parallel(
         for (int j = 0; j < 4; j ++) {
             if (abs(myDxx[i * 4 + j] - TestmyDxx[i][j]) > 0.00001f) {
                 cout << "myDxx[" << i << "][" << j << "] did not match! was " << myDxx[i * 4 + j] << " expected " << TestmyDxx[i][j] << endl;
-                return 1;
+                return procs;
             }
         }
     }
@@ -687,7 +694,7 @@ int   run_SimpleKernelized_Parallel(
         for (int j = 0; j < 4; j ++) {
             if (abs(myDyy[i * 4 + j] - TestmyDyy[i][j]) > 0.00001f) {
                 cout << "myDyy[" << i << "][" << j << "] did not match! was " << myDyy[i * 4 + j] << " expected " << TestmyDyy[i][j] << endl;
-                return 1;
+                return procs;
             }
         }
     }
@@ -698,7 +705,7 @@ int   run_SimpleKernelized_Parallel(
             for (int j = 0; j < numY; j ++) {
                 if (abs(myResultCopy[((o * numX) + i) * numY + j] - TestmyResult[o][i][j]) > 0.00001f) {
                     cout << "myResult[" << o << "][" << i << "][" << j << "] did not match! was " << myResultCopy[((o * numX) + i) * numY + j] << " expected " << TestmyResult[o][i][j] << endl;
-                    return 1;
+                    return procs;
                 }
             }
         }
@@ -710,17 +717,17 @@ int   run_SimpleKernelized_Parallel(
             for (int j = 0; j < numY; j ++) {
                 if (abs(myVarX[((t * numX) + i) * numY + j] - TestmyVarX[t][i][j]) > 0.00001f) {
                     cout << "myVarX[" << t << "][" << i << "][" << j << "] did not match! was " << myVarX[((t * numX) + i) * numY + j] << " expected " << TestmyVarX[t][i][j] << endl;
-                    return 1;
+                    return procs;
                 }
                 if (abs(myVarY[((t * numX) + i) * numY + j] - TestmyVarY[t][i][j]) > 0.00001f) {
                     cout << "myVarY[" << t << "][" << i << "][" << j << "] did not match! was " << myVarY[((t * numX) + i) * numY + j] << " expected " << TestmyVarY[t][i][j] << endl;
-                    return 1;
+                    return procs;
                 }
             }
         }
     }
 #endif
-    return 1;
+    return procs;
 }
 
 
