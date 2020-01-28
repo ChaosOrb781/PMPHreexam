e6cebaeb8b0df6215cd5e7821691e7ef9679628a
Chaos_Orb <sxr297@alumni.ku.dk>
2020-01-28 20:13:03 +0100

    Added to headerfile

--- CPU/ParaImpl/KernelAlgorithm.h
+++ CPU/ParaImpl/KernelAlgorithm.h
@@ -1,7 +1,21 @@
 #ifndef KERNEL_ALGORITHM
 #define KERNEL_ALGORITHM
 
-int run_SimpleKernel(
+int run_CPUKernel(
+                const uint   outer,
+                const uint   numX,
+                const uint   numY,
+                const uint   numT,
+                const REAL   s0,
+                const REAL   t, 
+                const REAL   alpha, 
+                const REAL   nu, 
+                const REAL   beta,
+                const uint   blocksize,
+                      REAL*  res   // [outer] RESULT
+);
+
+int run_GPUKernel(
                 const uint   outer,
                 const uint   numX,
                 const uint   numY,
