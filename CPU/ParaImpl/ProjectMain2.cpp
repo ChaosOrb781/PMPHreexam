89ee801b7c5c1333251f76b179b3ef910aca22e2
Chaos_Orb <sxr297@alumni.ku.dk>
2020-01-28 15:45:31 +0100

    SPlit file content

--- CPU/ParaImpl/ProjectMain2.cpp
+++ CPU/ParaImpl/ProjectMain2.cpp
@@ -1,9 +1,7 @@
 #include "OpenmpUtil.h"
 #include "ParseInput.h"
 #include "OriginalAlgorithm.h"
-#include "SimpleParallelAlgorithm.h"
-#include "InterchangedAlgorithm.h"
-#include "KernelizedAlgorithm.h"
+#include "DistributedAlgorithm.h"
 #include "KernelAlgorithm.h"
 
 #include "cuda.h"
@@ -103,8 +101,8 @@ int main()
         //RunTestOnProgram<funGPU>("Kernelized Dist Parallel", (fun)run_Kernelized_Rollback_Dist_Parallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
         //RunTestOnProgram<funGPU>("Kernelized Dist 2", (fun)run_Kernelized_Rollback_Dist_Alt, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
         //RunTestOnProgram<funGPU>("Kernelized Dist 2 Parallel", (fun)run_Kernelized_Rollback_Dist_Alt_Parallel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
-        RunTestOnProgram<funGPU>("Kernelized Dist 3", (fun)run_Kernelized_Rollback_Dist_Flat, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
-        RunTestOnProgram<funGPU>("Simple Kernel", (fun)run_SimpleKernel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
+        RunTestOnProgram<funGPU>("Kernelized Dist 3", (fun)run_Distributed, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
+        //RunTestOnProgram<funGPU>("Simple Kernel", (fun)run_SimpleKernel, res_original, originalStat, outer, numX, numY, numT, s0, t, alpha, nu, beta, Block);
     }
 
     return 0;
