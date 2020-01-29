c1b7549e28818231ec8d5e5284ec00c03ad87177
Chaos_Orb <sxr297@alumni.ku.dk>
2020-01-29 11:07:50 +0100

    Forgot to reoutcomment

--- CPU/ParaImpl/KernelAlgorithm.cu
+++ CPU/ParaImpl/KernelAlgorithm.cu
@@ -755,7 +755,7 @@ void rollback_Kernel_Test_CPU(
             for (int i = 0; i < outer * numY * numX; i++) {
                 if (std::abs(temp[i] - u_h_k[i]) > 0.0001 ) {
                     cout << "Rollback 4(u) index [" << i / (numY * numX) << "][" << (i % (numY * numX)) / numX << "][" << (i % (numY * numX)) % numX << "] failed to be equal, got " << u_h[i] << " expected " << temp[i] << endl;
-                    //exit(0);
+                    exit(0);
                 }
             }
             for (int i = 0; i < outer * numZ; i++) {
